7e78a3365b2acc74a7d8d1f552bfe3988b337732
Francesco Anania <fra.anania94@gmail.com>
2018-03-08 10:29:46 +0100

    Organizzazione struttura src, importazione librerie necessarie e creazione dei task di gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* jshint esversion:6 */
+
+const jshint = require('gulp-jshint');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('jshint', function() {
+    'use strict';
+    return gulp.src('./source/javascript/*.js').pipe(jshint()).pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('sass', function() {
+    'use strict';
+    gulp.src('./source/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./source/css/'));
+});
+
+gulp.task('sass-watch', function() {
+    'use strict';
+	gulp.watch('./source/sass/*.scss', ['sass']);
+});
