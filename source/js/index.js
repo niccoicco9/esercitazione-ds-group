7b14066dc3871519623704177dad73303b4272d1
Francesco Anania <fra.anania94@gmail.com>
2018-03-08 14:28:38 +0100

    Visualizzazione dei TODO

--- source/js/index.js
+++ source/js/index.js
@@ -2,29 +2,28 @@
 /* global $ */
 
 $(document).ready(function() {
-  'use strict';
+  "use strict";
   /* salvo in una variabile il numero di post da visualizzare sulla pagina */
-  const number = 4;
+  const number = 8;
   getPosts(number);
 });
 
 function getPosts(number) {
-  'use strict';
+  "use strict";
   for (var i = 0; i < number; i++) {
-    var randomInt = getRandomInt(0, 100);
-    $.getJSON(`https://jsonplaceholder.typicode.com/posts/${randomInt}`, function(data) {
-      var rowPost = $("#row-post").children();
-      console.log(data);
-      console.log(rowPost);
-    });
+	(function(i) {
+		var randomInt = getRandomInt(0, 200);
+		$.getJSON(`https://jsonplaceholder.typicode.com/todos/${randomInt}`, function(data) {
+			var rowPost = $("#row-post");
+			rowPost.append(`<div class='col-md-3'><div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>${data.title}</h5><p class='card-text'></p><a href='todo.html' class='btn btn-primary'>Visualizza</a></div></div></div>`);
+		});
+	})(i);
   }
 }
 
 function getRandomInt(min, max) {
-  'use strict';
+  "use strict";
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
 }
-
-
