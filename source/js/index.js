5e3ccb333fb54685d29ae87e26f98a77a81b5874
Francesco Anania <fra.anania94@gmail.com>
2018-03-08 12:40:42 +0100

    Creazione della funzione getPosts per recuperare n post dall'applicazione REST

--- source/js/index.js
+++ source/js/index.js
@@ -2,17 +2,37 @@
 /* global $ */
 
 $(document).ready(function() {
-  "use strict";
-  getPosts();
+  'use strict';
+  /* salvo in una variabile il numero di post da visualizzare sulla pagina */
+  const number = 5;
+  getPosts(number);
 });
 
-function getPosts() {
-    
-
-
-
-  "use strict";
-  fetch("https://jsonplaceholder.typicode.com/posts/5")
+function getPosts(number) {
+  'use strict';
+  var arrayPosts = new Array(number);
+  for (var i = 0; i < arrayPosts.length; i++) {
+    var randomInt = getRandomInt(0, 100);
+    fetch(`https://jsonplaceholder.typicode.com/posts/${randomInt}`)
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(function(response) {
+      
+      
+
+
+
+
+
+
+
+
+    });
+  }
+}
+
+function getRandomInt(min, max) {
+  'use strict';
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
 }
