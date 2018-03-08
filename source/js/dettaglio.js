abba31a409d8f2d262b52edd7fae26c8b5a47670
Francesco Anania <fra.anania94@gmail.com>
2018-03-08 16:01:52 +0100

    Formattazione documenti

--- source/js/dettaglio.js
+++ source/js/dettaglio.js
@@ -2,53 +2,54 @@
 /* global $ */
 
 $(document).ready(function() {
-    "use strict";
-    /* salvo in una variabile l'id del todo */
-    var todoId = window.location.hash.substring(1);
-    getTodo(todoId);
+  "use strict";
+  /* salvo in una variabile l'id del todo */
+  var todoId = window.location.hash.substring(1);
+  getTodo(todoId);
 
-    /* aggiungo event handler sul bottone #todo-action; quando cliccato viene aggiornata la risorsa*/
-    $('#todo-action').on( "click", function() {
-        fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                id: todoId,
-                completed: true
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        }).then(response => response.json()).then(function (json) {
-            console.log(json);
-            completaTask();
-        });
-    });
+  /* aggiungo event handler sul bottone #todo-action; quando cliccato viene aggiornata la risorsa*/
+  $("#todo-action").on("click", function() {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        id: todoId,
+        completed: true
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(response => response.json())
+      .then(function(json) {
+        console.log(json);
+        completaTask();
+      });
+  });
 });
-  
+
 function getTodo(id) {
-    "use strict";
-    $.getJSON(`https://jsonplaceholder.typicode.com/todos/${id}`, function (data) {
-        $('#todo-title').html(data.title);
-        var card = $("#todo-card");
-        if (data.completed === true) {
-            $('#todo-header').html('Hai completato il tuo task!');
-            $(".card-footer").remove();
-            card.removeClass('bg-light');
-            card.addClass('bg-success');
-        } else {
-            $('#todo-header').html('Il tuo task è ancora attivo');
-            card.removeClass('bg-light');
-            card.addClass('bg-danger');
-        }
-    });
+  "use strict";
+  $.getJSON(`https://jsonplaceholder.typicode.com/todos/${id}`, function(data) {
+    $("#todo-title").html(data.title);
+    var card = $("#todo-card");
+    if (data.completed === true) {
+      $("#todo-header").html("Hai completato il tuo task!");
+      $(".card-footer").remove();
+      card.removeClass("bg-light");
+      card.addClass("bg-success");
+    } else {
+      $("#todo-header").html("Il tuo task è ancora attivo");
+      card.removeClass("bg-light");
+      card.addClass("bg-danger");
+    }
+  });
 }
-  
 
 function completaTask() {
-    'use strict';
-    var card = $("#todo-card");
-    $('#todo-header').html('Hai completato il tuo task!');
-    $(".card-footer").remove();
-    card.removeClass('bg-danger');
-    card.addClass('bg-success');
+  "use strict";
+  var card = $("#todo-card");
+  $("#todo-header").html("Hai completato il tuo task!");
+  $(".card-footer").remove();
+  card.removeClass("bg-danger");
+  card.addClass("bg-success");
 }
