354ec6e92bee0d2d96ddf7fdbf3adb5382f13ca8
Francesco Anania <fra.anania94@gmail.com>
2018-03-09 12:58:33 +0100

    Inserimento modal-about

--- source/js/index.js
+++ source/js/index.js
@@ -3,7 +3,6 @@
 
 $(document).ready(function() {
 	'use strict';
-	
 	// Example starter JavaScript for disabling form submissions if there are invalid fields
 	window.addEventListener('load', function() {
 		// Fetch all the forms we want to apply custom Bootstrap validation styles to
