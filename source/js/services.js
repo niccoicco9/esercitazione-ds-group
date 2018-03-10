7818e91382626cfacd22f17026ddf88e49eecd8f
Hold94en <fra.anania94@gmail.com>
2018-03-10 18:49:52 +0100

    Terminata gestione utenti web storage

--- source/js/services.js
+++ source/js/services.js
@@ -1,5 +1,5 @@
 /* global $ */
-/* exported getData, login, signin */
+/* exported getData, signin, login, logout*/
 
 var url = 'https://jsonplaceholder.typicode.com/todos/';
 
@@ -13,12 +13,6 @@ function getData() {
             $.getJSON(url, callback);
         }
     };
-}
-
-function login(form) {
-	'use strict';
-	var email = form.elements[0].value;
-	var password = form.elements[1].value;
 }
 
 function signin(form) {
@@ -44,13 +38,45 @@ function signin(form) {
         }
         if (i !== -1) {
             usersArray.push(user);
-            localStorage.setItem("users", JSON.stringify(usersArray));
+			localStorage.setItem('users', JSON.stringify(usersArray));
+			window.alert('Registrazione effettuata con successo');
         } else {
-			alert("L'email inserita è già stata utilizzata");
+			window.alert('Email già utilizzata');
 		}
 	}
 }
 
+function login(form) {
+	'use strict';
+    var email = form.elements[0].value;
+    var password = form.elements[1].value;
+
+    if (localStorage.getItem('users') === null) {
+        window.alert('Indirizzo email non registrato');
+    } else {
+		var usersArray = JSON.parse(localStorage.getItem('users'));
+        var i = 0;
+        while (i >= 0 && i < usersArray.length) {
+            if (usersArray[i].email.trim() === email.trim() && usersArray[i].password === password) {
+                delete usersArray[i].password;
+				sessionStorage.setItem('onlineUser', JSON.stringify(usersArray[i]));
+				window.alert(email + " ha effettuato il login.");
+                i = -1;
+            } else {
+				i++;
+			}
+        }
+        if (i !== -1) {
+			window.alert('Indirizzo email o password errata');
+		}
+    }
+}
+
+function logout() {
+	'use strict';
+	sessionStorage.removeItem('onlineUser');
+	window.alert("Hai effettuato il logout.");
+}
 
 function getRandomInt(min, max) {
 	'use strict';
