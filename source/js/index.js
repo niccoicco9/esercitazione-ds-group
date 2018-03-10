560d25548416d7b696a19e02138e73706e6ba085
Hold94en <fra.anania94@gmail.com>
2018-03-10 18:18:50 +0100

    Refactoring del codice index.js

--- source/js/index.js
+++ source/js/index.js
@@ -1,28 +1,32 @@
-/*jshint esversion: 6 */
-/* global $ */
+/* global login, signin */
 
-$(document).ready(function() {
+window.addEventListener('load', function() {
+	'use strict';
+	addListeners();
+});
+
+function addListeners() {
 	'use strict';
 	// Example starter JavaScript for disabling form submissions if there are invalid fields
-	window.addEventListener('load', function() {
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = $('.needs-validation');
-		// Loop over them and prevent submission
-		for (var i = 0; i < forms.length; i++) {
-			forms[i].addEventListener('submit', function(event) {
-				if (this.checkValidity() === false) {
-					event.preventDefault();
-					event.stopPropagation();
-				} else {
-					if (this.id === 'form-login') {
-						login(this);
-					} else {
-						signin(this);
-					}
-				}
-				this.classList.add('was-validated');
-			}, false);
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.getElementsByClassName('needs-validation');
+	// Loop over them and prevent submission
+	for (var i = 0; i < forms.length; i++) {
+		forms[i].addEventListener('submit', checkForm, false);
+	}
+}
+
+function checkForm() {
+	alert(this.id);
+	if (this.checkValidity() === false) {
+		event.preventDefault();
+		event.stopPropagation();
+	} else {
+		if (this.id === 'form-login') {
+			login(this);
+		} else {
+			signin(this);
 		}
-	});
-});
-		
+	}
+	this.classList.add('was-validated');
+}
