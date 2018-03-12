19b77df1611536588a7c6b4fc9e993b372964543
Francesco Anania <fra.anania94@gmail.com>
2018-03-12 10:14:26 +0100

    Stilizzazione css

--- source/js/services.js
+++ source/js/services.js
@@ -102,7 +102,7 @@ function showTodos(data, userId) {
 		if (data[i].userId === userId) {
 			htmlString+="<tr class='table-light' id='table-row-" + data[i].id + "'>";
 			htmlString+="<th scope='row'>" + data[i].id + "</th><td>" + data[i].title + "</td>";
-			htmlString+="<td><a href='dettaglio.html#" + window.btoa(JSON.stringify(data[i])) + "' class='btn btn-light'>Visualizza dettaglio</a>";
+			htmlString+="<td class='td-center-content'><a href='dettaglio.html#" + window.btoa(JSON.stringify(data[i])) + "' class='btn btn-light'>Visualizza dettaglio</a>";
 			htmlString+="<button type='button' class='btn btn-danger'>Elimina todo</button></td>";
 			htmlString+="</tr>";
 		}
