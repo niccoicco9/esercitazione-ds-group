70afb4f9b1ea69b60f52842ac5c9b6dae2367634
Francesco Anania <fra.anania94@gmail.com>
2018-03-12 13:05:12 +0100

    Inserimento funzione showCreateTodo()

--- source/js/views.js
+++ source/js/views.js
@@ -18,24 +18,8 @@ function showTodos(data, userId) {
 }
 
 function showCreateTodo() {
-
-
-
-
+	'use strict';
+	var htmlString = document.getElementById('table-body-todos').innerHTML;
+	htmlString+="<tbody><tr><form><td></td><td><div class='form-group'><input type='text' class='form-control' id='input-todo' placeholder='Inserisci il titolo del nuovo todo'></div></td><td class='td-center-content'><button type='submit' class='btn btn-light'>Crea todo</button></td></form></tr></tbody>";
+	document.getElementById('table-body-todos').innerHTML = htmlString;
 }
-
-
-
-/*<tbody>
-	<tr>
-		<form>
-			<td></td>
-			<td>
-				<div class="form-group">
-					<input type="text" class="form-control" id="input-todo" placeholder="Titolo del nuovo todo">
-				</div>
-			</td>
-			<td class="td-center-content"><button type="submit" class="btn btn-light">Crea todo</button></td>
-		</form>
-	</tr>
-</tbody>*/
